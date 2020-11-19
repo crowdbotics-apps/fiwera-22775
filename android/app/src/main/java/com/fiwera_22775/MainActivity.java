5c9162bd61ed89757fb22f61e203c65974024235
Crowdbotics <team@crowdbotics.com>
2020-11-19 16:35:51 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/fiwera_22775/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fiwera_22775;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "fiwera_22775";
+    }
+}
